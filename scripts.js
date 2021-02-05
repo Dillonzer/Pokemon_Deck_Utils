9831b9f1ee7dfd76b70ad1fc2194fd8079349ac1
Dillonzer <dillonbast@gmail.com>
2021-02-05 10:03:03 -0600

    Getting the decklist loading working

--- scripts.js
+++ scripts.js
@@ -1,24 +1,91 @@
-function Decklist(pokemon, trainers, energy)
+function Decklist()
 {
-    this.Pokemon = pokemon
-    this.Trainers = trainers
-    this.Energy = energy
+    this.Pokemon = []
+    this.Trainers = []
+    this.Energy = []
+
+    this.addPokemon = function (newObject) {
+        this.Pokemon.push(newObject);
+    };
+    
+    this.addTrainers = function (newObject) {
+        this.Trainers.push(newObject);
+    };
+    
+    this.addEnergy = function (newObject) {
+        this.Energy.push(newObject);
+    };
 }
 
-//create a function to dynamically create table with 6 columns to show cards 
-function CreatePrizeSelectorTable() {   
-    
-    //CREATE DECKLIST HERE
-    /*var decklistText = document.getElementById('decklist').textContent
-    var decklist = CreateDecklistImages(decklistText)
+function DecklistItem(info, number)
+{
+    this.Info = info
+    this.Number = number
+}
 
+function Card(name, set, number, imageLink)
+{
+    this.Name = name
+    this.Set = set
+    this.Number = number
+    this.ImageLink = imageLink
+}
+
+function SubmitDecklist() {   
+    
+    var decklistText = document.getElementById('decklist').value;
+    CreateDecklistObject(decklistText)    
+}
+
+function CreateDecklistObject(decklistText)
+{
+    var decklist = new Decklist()
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({"decklist": decklistText});
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("https://dev-ptcg-api.herokuapp.com/prizeTracker/generateDecklist", requestOptions)
+    .then(response => {
+        return response.json()
+        })
+    .then(data => {
+        for(var i = 0; i < data["pokemon"].length; i++)
+        {
+            decklist.addPokemon(new DecklistItem(data["pokemon"][i].info, data["pokemon"][i].number))
+        }
+        
+        for(var i = 0; i < data["trainers"].length; i++)
+        {
+            decklist.addTrainers(new DecklistItem(data["trainers"][i].info, data["trainers"][i].number))
+        }
+        
+        for(var i = 0; i < data["energy"].length; i++)
+        {
+            decklist.addEnergy(new DecklistItem(data["energy"][i].info, data["energy"][i].number))
+        }        
+
+        CreatePrizeSelectorTable(decklist)
+    })
+    .catch(error => console.log('error', error));
+}
+
+async function CreatePrizeSelectorTable(decklist)
+{
     if(typeof decklist == 'undefined')
     {
         //show error message
+        alert("The decklist is invalid.")
         return
-    }*/
-    var decklist = new Decklist(["test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test",])
-
+    }
     var table = document.getElementById('prizeselectortable');
     
     while(table.rows.length > 0) {
@@ -30,9 +97,9 @@ function CreatePrizeSelectorTable() {
     var rowCnt = table.rows.length;
     var tr = table.insertRow(rowCnt);
 
-    for(poke in decklist.Pokemon)
+    for(var i = 0; i < decklist.Pokemon.length; i++)
     {
-        if(colCount==6)
+        if(colCount==9)
         {
             colCount = 0
             rowCnt = table.rows.length;
@@ -41,22 +108,97 @@ function CreatePrizeSelectorTable() {
 
         var td = document.createElement('td');
         td = tr.insertCell(colCount);
-        
-        var cardImage = document.createElement('img');//CREATE CARD IMAGE
-        var cardAmountSpan = document.createElement('span');//CREATE CARD AMOUNT SPAN
-        cardImage.src = "https://pkmn-tcg-api-images.sfo2.cdn.digitaloceanspaces.com/Vivid%20Voltage/en_US/aegislash%20v%20126.png"
-        cardImage.className = "card"
-        cardAmountSpan.innerHTML = "4"
-        td.appendChild(cardImage);
-        td.appendChild(cardAmountSpan);
+        await GetDecklistCardInformation(decklist.Pokemon[i].Info)
+        .then(card => {
+            var cardImage = document.createElement('img');//CREATE CARD IMAGE
+            var cardAmountSpan = document.createElement('span');//CREATE CARD AMOUNT SPAN
+            cardImage.src = card.ImageLink
+            cardImage.className = "card"
+            cardAmountSpan.innerHTML = decklist.Pokemon[i].Number
+            td.appendChild(cardImage);
+            td.appendChild(cardAmountSpan);
+    
+            colCount++
+        })
+    }
 
-        colCount++
+    for(var i = 0; i < decklist.Trainers.length; i++)
+    {
+        if(colCount==9)
+        {
+            colCount = 0
+            rowCnt = table.rows.length;
+            tr = table.insertRow(rowCnt);
+        }
+
+        var td = document.createElement('td');
+        td = tr.insertCell(colCount);
+        await GetDecklistCardInformation(decklist.Trainers[i].Info)
+        .then(card => {
+            var cardImage = document.createElement('img');//CREATE CARD IMAGE
+            var cardAmountSpan = document.createElement('span');//CREATE CARD AMOUNT SPAN
+            cardImage.src = card.ImageLink
+            cardImage.className = "card"
+            cardAmountSpan.innerHTML = decklist.Trainers[i].Number
+            td.appendChild(cardImage);
+            td.appendChild(cardAmountSpan);
+    
+            colCount++
+        })
+    }
+
+    for(var i = 0; i < decklist.Energy.length; i++)
+    {
+        if(colCount==9)
+        {
+            colCount = 0
+            rowCnt = table.rows.length;
+            tr = table.insertRow(rowCnt);
+        }
+
+        var td = document.createElement('td');
+        td = tr.insertCell(colCount);
+        await GetDecklistCardInformation(decklist.Energy[i].Info)
+        .then(card => {
+            var cardImage = document.createElement('img');//CREATE CARD IMAGE
+            var cardAmountSpan = document.createElement('span');//CREATE CARD AMOUNT SPAN
+            cardImage.src = card.ImageLink
+            cardImage.className = "card"
+            cardAmountSpan.innerHTML = decklist.Energy[i].Number
+            td.appendChild(cardImage);
+            td.appendChild(cardAmountSpan);
+    
+            colCount++
+        })
     }
 }
 
-function CreateDecklistImages(decklistText)
+function GetDecklistCardInformation(decklistCard)
 {
-    //call API
-    var decklist = new Decklist()
-    return decklist
+    var card = new Card()
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({"card":decklistCard});
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    return fetch("https://dev-ptcg-api.herokuapp.com/prizeTracker/getCardInformation", requestOptions)
+    .then(response => {
+        return response.json()
+        })
+    .then(data => {
+        card.Name = data["name"]
+        card.Number = data["number"]
+        card.Set = data["set"]["name"]
+        card.ImageLink = data["imageUrlHiRes"]
+
+        return card        
+    })
+    .catch(error => console.log('error', error));
 }
