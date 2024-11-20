335acde56e61ba6a4a9c502fbe7aeeac351a6c3d
Dillonzer <dillonzerapps@gmail.com>
2024-11-20 10:23:00 -0600

    adding test for topcut

--- azulgg/tournamentTracker.js
+++ azulgg/tournamentTracker.js
@@ -7,9 +7,13 @@ function Tournament(name, format, round)
   this.Ongoing = true;
 }
 
-var playerName = 'azulgg'
+var playerName = 'speedlemon'
 var tournamentId, tournamentObject, wins, losses, ties, timerInterval
+var standingsObject
 var intervalCleared = true
+var topcut = false
+var topcutNumber = -1
+var top32, top16, top8, top4, finals = false
 
 function setTournament() {
     url = "https://play.limitlesstcg.com/ext/dillonzer/init?username="+playerName+"&tournamentId="+tournamentId
@@ -26,6 +30,71 @@ function setTournament() {
 
         setTournamentInformation()
     });
+}
+
+function AreWeInTopcut(){
+  var settings = {
+    "url": "https://play.limitlesstcg.com/ext/live/tournament/"+tournamentId,
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    if(response.phaseType == "SINGLE_ELIMINATION" || response.phaseType == "SINGLE_BRACKET")
+    {
+      topcut = true      
+      standings()     
+    }
+    else
+    {
+      topcut = false
+    }
+  });
+}
+
+function WhatTopCutRound(usernameCheck, topNumber)
+{
+  var settings = {
+    "url": "https://play.limitlesstcg.com/ext/dillonzer/update?username="+usernameCheck+"&tournamentId="+tournamentId,
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    switch(topNumber) {
+      case 16:
+        top16 = response.player.active
+        break;
+      case 8:
+        top8 = response.player.active
+        break;
+      case 4:
+        top4 = response.player.active
+        break;
+      case 2:
+        finals = response.player.active
+        break;
+      default:
+    } 
+  });
+}
+
+function standings()
+{
+  var settings = {
+    "url": "https://play.limitlesstcg.com/ext/dillonzer/standings?tournamentId="+tournamentId,
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    standingsObject = response   
+    //calculate here to find what round we are in
+    WhatTopCutRound(standingsObject[16].username, 16)
+    WhatTopCutRound(standingsObject[8].username, 8)
+    WhatTopCutRound(standingsObject[4].username, 4)
+    WhatTopCutRound(standingsObject[2].username, 2) 
+  });
 }
 
 function start(){
@@ -72,12 +141,33 @@ var timerFunction = async function() {
       stringSeconds = seconds
     }
 
-    document.getElementById("round").textContent = "Round: " + currentRound + " - "+ minutes + ":" + stringSeconds
+    if(topcut)
+    {  
+      if(finals && !top4) 
+      {
+        document.getElementById("round").textContent = "Finals"
+      }  
+      else if(top4 && !top8) 
+      {
+        document.getElementById("round").textContent = "Top 4"
+      }
+      else if(top8 && !top16) 
+      {
+        document.getElementById("round").textContent = "Top 8"
+      }
+      else if(top16) 
+      {
+        document.getElementById("round").textContent = "Top 16"
+      }
+    }
+    else
+    {
+      document.getElementById("round").textContent = "Round " + currentRound + " - "+ minutes + ":" + stringSeconds
+    }
     
-    if (timeleft <= 0) {
+    if (timeleft <= 0 && !topcut) {
       clearInterval(timerInterval);    
       document.getElementById("round").textContent = "ROUND OVER"
-      updateInformation()
     }
 
   }
@@ -93,16 +183,12 @@ var updateInformation = function() {
       "method": "GET",
       "timeout": 0,
     };
+
+    AreWeInTopcut()
     
     $.ajax(settings).done(function (response) {
         tournamentObject.Round = response.tournament.round
-        tournamentObject.Ongoing = response.tournament.ongoing
-        
-
-        if(response.tournament.roundEnd != null)
-        {
-          tournamentObject.RoundEnd = new Date(response.tournament.roundEnd)
-        }
+        tournamentObject.Ongoing = response.tournament.ongoing       
         
 
         document.getElementById("record").textContent = "W/L/T: " + response.player.record.wins + "-" + response.player.record.losses + "-" + response.player.record.ties
@@ -110,27 +196,31 @@ var updateInformation = function() {
     
         if(response.tournament.roundEnd != null)
         {
+          tournamentObject.RoundEnd = new Date(response.tournament.roundEnd)
+          
           if(response.match.completed)
           {
+            console.log("Match Completed")
             if(!intervalCleared)
             {
                 clearInterval(timerInterval)
                 intervalCleared = true
             }
-            document.getElementById("round").textContent = "Round " + (tournamentObject.Round + 1) + " Up Next"
+            if(!topcut)
+            {
+                document.getElementById("round").textContent = "Round " + (tournamentObject.Round + 1) + " Up Next"
+            }
           }
           else
           {
-            if(intervalCleared)
-            {            
-                intervalCleared = false
-                timerInterval = setInterval(timerFunction, 1100)
-                tournamentObject.RoundEnd = new Date(response.tournament.roundEnd)
-            }
+            tournamentObject.RoundEnd = new Date(response.tournament.roundEnd)
+            clearInterval(timerInterval)
+            intervalCleared = false
+            setInterval(timerFunction, 1100)
           }
         }
         else
-        {        
+        {    
           document.getElementById("round").textContent = "Round " + tournamentObject.Round
         }
       
